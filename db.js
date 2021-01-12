d108625d23adf85b17a1f0f71d0de45a6f8668c1
Charli <charlispilecki@gmail.com>
2021-01-11 23:50:57 -0600

    Add db functions

--- db.js
+++ db.js
@@ -9,37 +9,79 @@ var connection = mysql.createConnection({
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
-
-function listEmployees() {
-    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-        console.log('The solution is: ', results[0].solution);
-      });
+exports.disconnect = function () {
+  connection.end()
 }
 
-function listDepartments() {
-
+exports.getEmployees = function (callback) {
+    connection.query('select first_name, last_name, id from employee', function (error, results, fields) {
+      if (error) throw error
+      callback(results)
+    });
 }
 
-function listRoles() {
-
+exports.getDepartments = function (callback) {
+    connection.query('select name, id from department', function (error, results, fields) {
+      if (error) throw error
+      callback(results)
+    });
 }
 
-function addEmployee() {
-
+exports.getRoles = function (callback) {
+    connection.query('select title, id from role', function (error, results, fields) {
+      if (error) throw error
+      callback(results)
+    });
 }
 
-function addDepartment() {
-
+exports.listEmployees = function (callback) {
+    connection.query('select * from employee', function (error, results, fields) {
+      if (error) throw error
+      console.table(results)
+      callback()
+    });
 }
 
-function addRole() {
-
+exports.listDepartments = function (callback) {
+  connection.query('select * from department', function (error, results, fields) {
+    if (error) throw error
+    console.table(results)
+    callback()
+  });
 }
 
-function updateRoleForEmployee() {
+exports.listRoles = function (callback) {
+  connection.query('select * from role', function (error, results, fields) {
+    if (error) throw error
+    console.table(results)
+    callback()
+  });
+}
 
+exports.addEmployee = function (first_name, last_name, role_id, manager_id, callback) {
+  connection.query('insert into employee (first_name, last_name, role_id, manager_id) values (?, ?, ?, ?)', [first_name, last_name, role_id, manager_id], function (error, results, fields) {
+    if (error) throw error
+    callback()
+  });
+}
+
+exports.addDepartment = function (name, callback) {
+  connection.query('insert into department (name) values (?)', [name], function (error, results, fields) {
+    if (error) throw error
+    callback()
+  });
+}
+
+exports.addRole = function (title, salary, department_id, callback) {
+  connection.query('insert into role (title, salary, department_id) values (?, ?, ?)', [title, salary, department_id], function (error, results, fields) {
+    if (error) throw error
+    callback()
+  });
+}
+
+exports.updateRoleForEmployee = function (employee_id, role_id, callback) {
+  connection.query('update employee set role_id = ? where id = ?', [role_id, employee_id], function (error, results, fields) {
+    if (error) throw error
+    callback()
+  });
 }
--- prompts.js
+++ prompts.js
@@ -15,7 +15,8 @@ function promptActions() {
                     "Add an employee",
                     "Add a department",
                     "Add a role",
-                    "Update an employee's role"
+                    "Update an employee's role",
+                    "I'm done"
                 ]
             }
         ])
@@ -23,22 +24,28 @@ function promptActions() {
 
             switch (answers.action) {
                 case "View all employees":
-                    db.listEmployees()
+                    db.listEmployees(promptActions)
                     break
                 case "View all all departments":
-                    db.listDepartments()
+                    db.listDepartments(promptActions)
                     break
                 case "View all roles":
-                    db.listRoles()
+                    db.listRoles(promptActions)
                     break
                 case "Add an employee":
+                    promptAddEmployee()
                     break
                 case "Add a department":
                     promptAddDepartment()
                     break
                 case "Add a role":
+                    promptAddRole()
                     break
                 case "Update an employee's role":
+                    promptUpdateEmployeeRole()
+                    break
+                default:
+                    db.disconnect()
                     break
             }
         })
@@ -47,74 +54,155 @@ function promptActions() {
 
 function promptAddDepartment() {
     inquirer
-    .prompt([
-        {
-            "name": "name",
-            "message": "What is the department's name?"
-        }
-    ])
-    .then(answers => {
-
-    })
+        .prompt([
+            {
+                "name": "name",
+                "message": "What is the department's name?"
+            }
+        ])
+        .then(answers => {
+            db.addDepartment(answers.name, promptActions)
+        })
 }
 
 function promptAddRole() {
-    inquirer
-    .prompt([
-        {
-            "name": "title",
-            "message": "What is the role's title?"
-        },
-        {
-            "name": "salary",
-            "message": "What is the role's salary?"
-        },
-        {
-            "name": "department",
-            "message": "What is the role's department?"
-        }
-    ])
-    .then(answers => {
+    let departmentChoices = []
+    db.getDepartments(results => {
+
+        results.forEach(result => {
+            departmentChoices.push({
+                name: result.name,
+                value: result.id
+            })
+        })
+
+        inquirer
+            .prompt([
+                {
+                    "name": "title",
+                    "message": "What is the role's title?"
+                },
+                {
+                    "name": "salary",
+                    "message": "What is the role's salary?"
+                },
+                {
+                    "name": "department",
+                    "message": "What is the role's department?",
+                    "type": "list",
+                    "choices": departmentChoices
+                }
+            ])
+            .then(answers => {
+                db.addRole(answers.title, answers.salary, answers.department, promptActions)
+            })
 
     })
 }
 
 function promptAddEmployee() {
-    inquirer
-    .prompt([
-        {
-            "name": "firstName",
-            "message": "What is the employee's first name?"
-        },
-        {
-            "name": "lastName",
-            "message": "What is the employee's last name?"
-        },
-        {
-            "name": "role",
-            "message": "What is the employee's role?"
-        },
-        {
-            "name": "manager",
-            "message": "Who is the employee's manager?"
-        }
-    ])
-    .then(answers => {
+    let managerChoices = []
+    let roleChoices = []
+
+    db.getEmployees(results => {
+
+        results.forEach(result => {
+            managerChoices.push({
+                name: result.first_name + ' ' + result.last_name,
+                value: result.id
+            })
+        })
+
+        db.getRoles(roleResults => {
+
+            roleResults.forEach(roleResult => {
+                roleChoices.push({
+                    name: roleResult.title,
+                    value: roleResult.id
+                })
+            })
+
+            inquirer
+                .prompt([
+                    {
+                        "name": "firstName",
+                        "message": "What is the employee's first name?"
+                    },
+                    {
+                        "name": "lastName",
+                        "message": "What is the employee's last name?"
+                    },
+                    {
+                        "name": "role",
+                        "message": "What is the employee's role?",
+                        "type": "list",
+                        "choices": roleChoices
+                    },
+                    {
+                        "name": "manager",
+                        "message": "Who is the employee's manager?",
+                        "type": "list",
+                        "choices": managerChoices
+                    }
+                ])
+                .then(answers => {
+                    db.addEmployee(answers.firstName, answers.lastName, answers.role, answers.manager, promptActions)
+                })
+
+
+        })
 
     })
+
 }
 
 function promptUpdateEmployeeRole() {
-    inquirer
-    .prompt([
-        {
-            "name": "role",
-            "message": "What is the employee's new role?"
-        }
-    ])
-    .then(answers => {
+    let employeeChoices = []
+    let roleChoices = []
+    db.getEmployees(results => {
+
+        results.forEach(result => {
+            employeeChoices.push({
+                name: result.first_name + ' ' + result.last_name,
+                value: result.id
+            })
+        })
+
+        db.getRoles(roleResults => {
+
+            roleResults.forEach(roleResult => {
+                roleChoices.push({
+                    name: roleResult.title,
+                    value: roleResult.id
+                })
+            })
+
+            inquirer
+            .prompt([
+                {
+                    "name": "employee",
+                    "message": "Which employee do you want to update?",
+                    "type": "list",
+                    "choices": employeeChoices
+                },
+                {
+                    "name": "role",
+                    "message": "What is the employee's new role?",
+                    "type": "list",
+                    "choices": roleChoices
+                }
+            ])
+            .then(answers => {
+                db.updateRoleForEmployee(answers.employee, answers.role, promptActions)
+            })
+
+        })
+
+        
+
 
     })
+
 }
 
 module.exports = {
